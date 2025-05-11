5ee683934bbc2af4e5b0bc0cf60991807db92c1d
a-fan-l <ywtw7890@gmail.com>
2025-05-11 14:54:19 +0800

    fix: bug

--- src/mastra/index.ts
+++ src/mastra/index.ts
@@ -4,10 +4,13 @@ import { LibSQLStore } from '@mastra/libsql';
 
 import { frontendInterviewAgent } from './agents/frontendInterviewAgent.js';
 import { frontendInterviewWorkflow } from './workflows/frontendInterviewWorkflow.js';
+import { codeReviewAgent } from './agents/codeReviewAgent.js';
+import { weatherAgent } from './agents/weatherAgent.js';
+import { cryptoAgent } from './agents/cryptoAgent.js';
 
 export const mastra = new Mastra({
   workflows: { frontendInterviewWorkflow },
-  agents: { frontendInterviewAgent },
+  agents: { frontendInterviewAgent, codeReviewAgent, weatherAgent, cryptoAgent},
   storage: new LibSQLStore({
     // stores telemetry, evals, ... into memory storage, if it needs to persist, change to file:../mastra.db
     url: ":memory:",
